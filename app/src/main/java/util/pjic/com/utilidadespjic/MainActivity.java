1c6a5d66e22d2c1b2302c853e224022615988acb
Juan David Roldán Ceballos <juan.roldan@simple-se.co>
2019-01-15 22:51:49 -0500

    Create java configuration

--- app/src/main/java/util/pjic/com/utilidadespjic/MainActivity.java
+++ app/src/main/java/util/pjic/com/utilidadespjic/MainActivity.java
@@ -1,11 +1,18 @@
 package util.pjic.com.utilidadespjic;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import util.pjic.com.utilidadespjic.adapters.NotaAdapter;
 import util.pjic.com.utilidadespjic.models.Nota;
@@ -15,6 +22,10 @@ public class MainActivity extends AppCompatActivity {
     private NotaAdapter notaAdapter;
     private RecyclerView recyclerView;
     private ArrayList<Nota> notas = new ArrayList<>();
+    private Button btnCalculate;
+    private FloatingActionButton floatingActionButton;
+    private TextView textView;
+    private float total;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,11 +36,30 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setHasFixedSize(true);
         notaAdapter = new NotaAdapter(this, notas);
-        Nota nota = new Nota();
+        final Nota nota = new Nota();
         nota.setNote(4);
-        nota.setNote(4);
+        nota.setPercentage(4);
         notas.add(nota);
-
+        textView = findViewById(R.id.textView5);
         recyclerView.setAdapter(notaAdapter);
+        btnCalculate = findViewById(R.id.btnCalculate);
+        btnCalculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                for (Nota nota : notas) {
+                   total = nota.getNote() * nota.getPercentage();
+                }
+                textView.setText(String.valueOf((int) total));
+            }
+        });
+
+        floatingActionButton = findViewById(R.id.floatBtnAdd);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                notas.add(new Nota());
+                notaAdapter.notifyDataSetChanged();
+            }
+        });
     }
 }
