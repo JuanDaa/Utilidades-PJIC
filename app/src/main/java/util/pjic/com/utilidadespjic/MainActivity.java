1d9a99428a4e6756bfa72dbc68511b75e4da4850
juan-roldan <jr@sempli.co>
2019-01-21 17:33:20 -0500

    Add FUNCTIONS

--- app/src/main/java/util/pjic/com/utilidadespjic/MainActivity.java
+++ app/src/main/java/util/pjic/com/utilidadespjic/MainActivity.java
@@ -1,5 +1,6 @@
 package util.pjic.com.utilidadespjic;
 
+import android.app.Dialog;
 import android.content.DialogInterface;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AlertDialog;
@@ -7,8 +8,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.DisplayMetrics;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -26,8 +29,11 @@ public class MainActivity extends AppCompatActivity {
     private ArrayList<Nota> notas = new ArrayList<>();
     private Button btnCalculate;
     private FloatingActionButton floatingActionButton;
-    private TextView textView;
     private float total;
+    private float totalPercentage;
+    private float failureToWinPercentage;
+    private double failureToWin;
+    private Dialog dialogCalculate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,20 +48,61 @@ public class MainActivity extends AppCompatActivity {
         nota.setNote(null);
         nota.setPercentage(null);
         notas.add(nota);
-        textView = findViewById(R.id.textView5);
         recyclerView.setAdapter(notaAdapter);
         btnCalculate = findViewById(R.id.btnCalculate);
         btnCalculate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                int imageToView;
+                String messageText = "";
+                String messageResult = "";
+
                 for (Nota nota : notas) {
-                    if (nota.getNote() == null || nota.getPercentage() == null) {
+                    if (nota.getNote() == null) {
                         return;
                     }
+
+                    if (nota.getPercentage() == null) {
+                        return;
+                    }
+                    totalPercentage += nota.getPercentage();
                     float percentage = nota.getPercentage() / 100;
                     total += percentage * (nota.getNote());
                 }
-                textView.setText(String.valueOf((total)));
+
+                if (totalPercentage == 100) {
+                    if (total == 5.0) {
+                        imageToView = R.drawable.ic_star;
+                        messageText = "Tu nota final es de" + " " + total;
+                        messageResult = "Relajate";
+                    } else {
+                        imageToView = R.drawable.ic_depression;
+                        failureToWin = 5.0 - total;
+                        messageText = "tu nota final fue de" + " " + total;
+                        messageResult = "Lo sentimos, no alcanzaste la meta";
+                    }
+                } else {
+                    failureToWinPercentage = 100 - totalPercentage;
+
+                    if (total > 4.5) {
+                        imageToView = R.drawable.ic_geek;
+                        messageText = "No necesitas sacar nota para el final";
+                        messageResult = "Pero esfuérzate para dejarla altica";
+                    } else {
+                        failureToWin = 5.0 - total;
+                        if (failureToWin > 5.0) {
+                            imageToView = R.drawable.ic_depression;
+                            messageText = "Ya no alcanzaras la nota";
+                            messageResult = "Lo sentimos";
+                        }else {
+                            imageToView = R.drawable.ic_surprise;
+                            messageText = "Te hace falta sacar" + " " + failureToWin + "En el " + failureToWinPercentage + " restante";
+                            messageResult = "No te descuides";
+                        }
+                    }
+                }
+                openCalculateDialog(imageToView, messageText, messageResult);
                 total = 0;
             }
         });
@@ -107,6 +154,34 @@ public class MainActivity extends AppCompatActivity {
                         notaAdapter.notifyDataSetChanged();
                     }
                 }
+            }
+        });
+    }
+
+    public void openCalculateDialog(int imageToView, String messaget, String messageR) {
+        dialogCalculate = new Dialog(MainActivity.this);
+        dialogCalculate.setContentView(R.layout.calculate_note_dialog);
+
+        ImageView imageResult = dialogCalculate.findViewById(R.id.imageResult);
+        TextView messageText = dialogCalculate.findViewById(R.id.messageText);
+        TextView messageResult = dialogCalculate.findViewById(R.id.messageResult);
+
+        dialogCalculate.setTitle("Tus resultados");
+        imageResult.setImageResource(imageToView);
+        messageText.setText(messaget);
+        messageResult.setText(messageR);
+        DisplayMetrics metrics = getResources().getDisplayMetrics();
+        int width = metrics.widthPixels;
+        int height = metrics.heightPixels;
+        dialogCalculate.getWindow().setLayout((6 * width)/7, (4 * height)/5);
+        dialogCalculate.show();
+
+
+        Button closeButton = dialogCalculate.findViewById(R.id.closeButton);
+        closeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialogCalculate.dismiss();
             }
         });
     }
